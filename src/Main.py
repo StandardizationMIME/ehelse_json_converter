1500383906c10f873acbc4de441de9abcbffc163
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-23 14:00:42 +0200

    Added main file

---
+++ src/Main.py
@@ -0,0 +1,9 @@
+from Tkinter import *
+class MainView:
+    def __init__(self, master):
+        # TODO: implement GUI components
+        pass
+
+if __name__ == '__main__':
+    # Initiate main
+    pass
