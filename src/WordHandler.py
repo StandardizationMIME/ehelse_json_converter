38396dc382b54884661ef64747f0e1b314155e0e
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-24 14:07:36 +0200

    Added fields to addDocument

--- src/WordHandler.py
+++ src/WordHandler.py
@@ -1,6 +1,6 @@
 from docx import Document
 
-class WordHanlder:
+class WordHandler:
     HEADING_2 = 2
     HEADING_3 = 3
     
@@ -12,9 +12,18 @@ class WordHanlder:
     def add_topic(self, topic):
         self.word_document.add_heading(topic['title'], level=self.HEADING_2)
 
-    def add_document(self, document):
+    def add_document(self, document, document_fields):
+        # Title
         self.word_document.add_heading(document['title'], level= self.HEADING_3)
+        # Description
         self.word_document.add_paragraph(document['description'])
+        # Fields
+        table = self.word_document.add_table(1,2)
+        for document_field in document_fields:
+            cells = table.add_row().cells
+            cells[0].text = document_field['name']
+            cells[1].text = document_field['value']
 
     def save_word_document(self, file_path, file_name):
         self.word_document.save("%s/%s.docx" % (file_path, file_name))
+
