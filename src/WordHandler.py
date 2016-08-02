111942f870472f02b11c71dc0af001dab863fee1
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-08-02 15:23:32 +0200

    Added status, his and links

--- src/WordHandler.py
+++ src/WordHandler.py
@@ -1,9 +1,11 @@
-from docx import Document
+from docx import *  #TODO: check if we can import less
+from docx.oxml.shared import *
 
 
 class WordHandler:
     HEADING_2 = 2
     HEADING_3 = 3
+    HEADING_4 = 4
 
     word_document = None
 
@@ -14,17 +16,91 @@ class WordHandler:
         self.word_document.add_heading(topic['title'], level=self.HEADING_2)
 
     def add_document(self, document, inputHandler):
+        document_id = document["id"]
         # Title
         self.word_document.add_heading(document['title'], level=self.HEADING_3)
         # Description
         self.word_document.add_paragraph(document['description'])
-        # Fields
-        document_fields = inputHandler.get_field_list_by_document_id(document["id"])
-        table = self.word_document.add_table(1, 2)
+        # Field table
+        document_fields = inputHandler.get_field_list_by_document_id(document_id)
+        table = self.word_document.add_table(0, 2)
+
+        # -- Status
+
+        cells = table.add_row().cells
+        cells[0].text = 'Status'
+        cells[1].text = inputHandler.getStatusById(document['statusId'])['name']
+
+        # -- HIS number
+        if (document['hisNumber']):
+
+            cells = table.add_row().cells
+            cells[0].text = 'HIS-nummer'
+            cells[1].text = document['hisNumber']
+
+        # -- Document fields
         for document_field in document_fields:
             cells = table.add_row().cells
             cells[0].text = document_field['name']
             cells[1].text = document_field['value']
 
+        # Links
+        link_category_dict = inputHandler.get_link_category_dict_by_document_id(document_id)
+
+        for link_category_id, link_category in link_category_dict.iteritems():  # Loop through all link categories of the document
+            self.word_document.add_heading(link_category['name'], level=self.HEADING_4)
+            for link in inputHandler.get_links_by_link_category_id_and_document_id(link_category_id, document_id): # for each link in current category
+                link_paragraph = self.word_document.add_paragraph(style='ListBullet') #TODO: deprecated, check if therese is a new way
+                self.add_hyperlink(link_paragraph, link['url'], link['text'])   #TODO: Make sure url starts with http/https else, looking for file
+
+
     def save_word_document(self, file_path, file_name):
         self.word_document.save("%s/%s.docx" % (file_path, file_name))
+
+    # Credit: https://github.com/rushton3179
+    def add_hyperlink(self, paragraph, url, text):
+        """
+        A function that places a hyperlink within a paragraph object.
+
+        :param paragraph: The paragraph we are adding the hyperlink to.
+        :param url: A string containing the required url
+        :param text: The text displayed for the url
+        :return: A Run object containing the hyperlink
+        """
+
+        # This gets access to the document.xml.rels file and gets a new relation id value
+        part = paragraph.part
+        r_id = part.relate_to(url, RT.HYPERLINK, is_external=True)
+
+        # Create the w:hyperlink tag and add needed values
+        hyperlink = OxmlElement('w:hyperlink')
+        hyperlink.set(qn('r:id'), r_id, )
+        hyperlink.set(qn('w:history'), '1')
+
+        # Create a w:r element
+        new_run = OxmlElement('w:r')
+
+        # Create a new w:rPr element
+        rPr = OxmlElement('w:rPr')
+
+        # Create a w:rStyle element, note this currently does not add the hyperlink style as its not in
+        # the default template, I have left it here in case someone uses one that has the style in it
+        rStyle = OxmlElement('w:rStyle')
+        rStyle.set(qn('w:val'), 'Hyperlink')
+
+        # Join all the xml elements together add add the required text to the w:r element
+        rPr.append(rStyle)
+        new_run.append(rPr)
+        new_run.text = text
+        hyperlink.append(new_run)
+
+        # Create a new Run object and add the hyperlink into it
+        r = paragraph.add_run()
+        r._r.append(hyperlink)
+
+        # A workaround for the lack of a hyperlink style (doesn't go purple after using the link)
+        # Delete this if using a template that has the hyperlink style in it
+        # r.font.color.theme_color = MSO_THEME_COLOR_INDEX.HYPERLINK
+        r.font.underline = True
+
+        return r
