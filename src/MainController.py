0b7d7fc76815f5238dbca63335ccb914a58b954f
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-08-04 12:39:16 +0200

    Added basic error handling
    * Added error for invalid file path
    * Added error for invalid file format
    * Added error for error in Word file creation

--- src/MainController.py
+++ src/MainController.py
@@ -1,5 +1,6 @@
 from InputHandler import *
 from MainView import *
+from Messages import *
 from WordHandler import *
 import tkFileDialog
 
@@ -21,20 +22,25 @@ class MainController:
         self.__clear_error_message()
         if self.input_path:
             self.output_path = tkFileDialog.asksaveasfilename(defaultextension=".docx")
-            if self.output_path is None:  # asksaveasfile return `None` if dialog closed with "cancel".
-                print 'error, idk'
-                return
-
-            self.__generate_word_document(self.input_path, self.output_path)
+            if self.output_path:
+                try:
+                    self.__generate_word_document(self.input_path, self.output_path)
+                except Exception as e: # TODO: handle more specific exceptions
+                    print e
+                    self.main_view.set_error_message(Messages.ERROR_INVALID_INPUT_CONTENT)
         else:
-            # Error feedback
-            self.main_view.set_error_message('Error!')
-            print 'No file selected'
+            self.main_view.set_error_message(Messages.ERROR_NO_INPUT_PATH_SELECTED)
 
     def uplpoad(self):
-        self.input_path = tkFileDialog.askopenfilename(parent=self.main_view, initialdir="/", title='Last opp JSON-fil')
+        self.__clear_error_message()
+        path = tkFileDialog.askopenfilename(parent=self.main_view, initialdir="/", title='Last opp JSON-fil')
         self.main_view.set_input_path('')
-        if self.input_path:
+        if not path:
+            pass
+        elif self.__get_file_extension(path).lower() != 'json':
+            self.main_view.set_error_message(Messages.ERROR_INVALID_FILE_FORMAT)
+        else:   # Valid path
+            self.input_path = path
             self.main_view.set_input_path(self.input_path)
 
     def __clear_error_message(self):
@@ -72,3 +78,8 @@ class MainController:
         file_name_with_extension = full_path[len(self.__get_file_path(full_path)) + 1:]
         file_extension_index = file_name_with_extension.rfind('.docx')
         return file_name_with_extension[:file_extension_index]
+
+    def __get_file_extension(self, full_path):
+        file_name_with_extension = full_path[len(self.__get_file_path(full_path)) + 1:]
+        file_extension_index = file_name_with_extension.rfind('.')
+        return file_name_with_extension[file_extension_index+1 :]
