f69e5443f5848b8ca9da626c0e70d8d0d8896800
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-08-11 14:22:32 +0200

    Fixed bug with status name

--- src/ExportContent.py
+++ src/ExportContent.py
@@ -34,14 +34,14 @@ class ExportContent:
 
             document_object['title'] = document['title']
             document_object['description'] = document['description']
-            document_object['status'] = input_handler.get_status_name_by_id(document_id)
+            document_object['status'] = input_handler.get_status_name_by_id(document['statusId'])
 
             # Fields
             document_object['fields'] = input_handler.get_field_list_by_document_id(document_id)
 
             # Target groups
-            #document_object['mandatoryList'] = []
-            '''
+            document_object['mandatoryList'] = []
+
             mandatory_dict = input_handler.get_mandatory_dict_on_document_id(document_id)
             for mandataory_id, mandataory in mandatory_dict.iteritems():
                 target_groups = []
@@ -49,14 +49,18 @@ class ExportContent:
                                                                                                 document_id):
                     target_groups.append({
                         'name': input_handler.getTargetGroupById(target_group['targetGroupId'])['name'], # Name of target group
-                        'acton': input_handler.get_action_name_by_id(target_group['actionId'])
+                        'action': input_handler.get_action_name_by_id(target_group['actionId'])
                     })
 
+                print 'mandatroyId ' + mandataory_id + ' ' + mandataory['name']
                 document_object['mandatoryList'].append({
                     'name' : mandataory['name'],
-                    'targetGroups': target_groups
+                    'targetGroups': target_groups,
+                    'hjemmel': document['hjemmel'] if (mandataory_id == 1 and document['hjemmel'] is not None) else '',
+                    'decidedBy':
+                        document['decidedBy'] if (mandataory_id == 1 and document['decidedBy'] is not None) else ''
+
                 })
-            '''
 
             '''
             print input_handler.get_target_group_values_by_target_group_id_and_document_id()
