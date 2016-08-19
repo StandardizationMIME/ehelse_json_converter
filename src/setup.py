a82078f71e98bb04e5a5cbac2fec774581e11967
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-08-19 11:15:28 +0200

    Updatet template and changed success message

--- src/setup.py
+++ src/setup.py
@@ -7,6 +7,5 @@ path_to_icon = r"hdir.ico"
 setup(options={"py2exe": {"packages": packages}
                },
       windows=[{"script": 'Program.py'
-                # "icon_resources": [(1, path_to_icon)]
                 }]
     )
