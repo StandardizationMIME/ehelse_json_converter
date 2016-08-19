10efa63156e1ba71926c66c823cf02ab45c7b856
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-08-19 15:19:42 +0200

    Removed path_to_icon from setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 import py2exe
 
 packages = ['Tkinter', 'tkFileDialog', 'docx', 'docx.shared', 'docx.oxml.shared', 'docxtpl', 'lxml._elementpath', 'docx.enum.text']
-path_to_icon = r"hdir.ico"
 
 setup(options={"py2exe": {"packages": packages}
                },
